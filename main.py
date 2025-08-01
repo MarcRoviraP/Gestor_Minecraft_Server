366c955d8ede76cb5f7f56a6011143fe119363fe
MarcRoiraP <marcrovira2eso1@gmail.com>
2025-08-01 22:57:04 +0200

    Enhance main window UI and add server configuration features
    - Updated main window dimensions for better layout.
    - Moved "Create Server" button to a more accessible position.
    - Introduced a new widget for configuring server properties, including:
      - Server port, name, player limit, and chunk settings.
      - Options for online mode, whitelist, and PVP settings.
      - Difficulty and game mode selection with radio buttons.
      - Added functionality to save server properties.
    - Added a new icon for the application.

--- main.py
+++ main.py
@@ -7,26 +7,226 @@ import mc_server_utils
 import os
 import subprocess
 import shutil
-
+from functools import partial
+import json
+import requests
+import uuid
 
 base_path = os.path.join(os.path.expanduser("~"), "MinecraftServers")
 class Window(QMainWindow):
     def __init__(self,parent=None):
         super().__init__(parent)
+        self.lastServer = ""
         self.setWindowTitle("Main Window")
         self.main_window = Ui_MainWindow()
         self.main_window.setupUi(self)
+        
+        self.difficulty_radio_group = QButtonGroup(self)
+        self.difficulty_radio_group.addButton(self.main_window.peacefulBtn, 0)
+        self.difficulty_radio_group.addButton(self.main_window.easyBtn, 1)
+        self.difficulty_radio_group.addButton(self.main_window.normalBtn, 2)
+        self.difficulty_radio_group.addButton(self.main_window.hardBtn, 3)
+        
+        
+        self.gamemode_radio_group = QButtonGroup(self)
+        self.gamemode_radio_group.addButton(self.main_window.survivalBtn, 0)
+        self.gamemode_radio_group.addButton(self.main_window.creativeBtn, 1)
+        self.gamemode_radio_group.addButton(self.main_window.adventureBtn, 2)
+        self.gamemode_radio_group.addButton(self.main_window.spectatorBtn, 3)
+
+        # Whitelist 
+        self.main_window.configurePropertiesWidget.setVisible(False)
+        self.main_window.widgetWhiteList.setVisible(self.main_window.Whitelist.isChecked())
+        def on_whitelist_toggled(checked):
+            if checked:
+                self.reloadWhiteList()
+            self.main_window.widgetWhiteList.setVisible(checked)
+        self.main_window.Whitelist.toggled.connect(on_whitelist_toggled)
+
         #Abrir dialogo de crear servidor
         self.main_window.createServerBtn.clicked.connect(self.spawnDialog)
         
         # Recargar servidores al iniciar
         self.reloadServers()
         
-        
-        #Crear directorio base si no existe
-        self.crearBasePath()
+        # Add player list
+        self.main_window.buttonAddWhiteList.clicked.connect(self.insertUserWhiteList)
+        #Save properties
+        self.main_window.saveProperties.clicked.connect(self.saveProperties)
 
+        #Crear directorio base si no existe
+        self.crearBaseFolders()
+
+    def saveProperties(self):
+        ruta = os.path.join(base_path, "servers", self.lastServer)
+        if not os.path.exists(ruta):
+            print(f"El servidor {self.lastServer} no existe.")
+            return
+
+        properties_path = os.path.join(ruta, "server.properties")
+        new_properties = {
+            "difficulty": self.difficulty_radio_group.checkedButton().text().lower(),
+            "gamemode": self.gamemode_radio_group.checkedButton().text().lower(),
+            "max-players": str(self.main_window.players.value()),
+            "view-distance": str(self.main_window.chunks.value()),
+            "server-port": str(self.main_window.portNumber.value()),
+            "motd": self.main_window.serverNameEdit.text(),
+            "online-mode": "true" if self.main_window.onlineMode.isChecked() else "false",
+            "white-list": "true" if self.main_window.Whitelist.isChecked() else "false",
+            "pvp": "true" if self.main_window.pvp.isChecked() else "false"
+        }
+
+        # Leer propiedades existentes
+        existing = {}
+        if os.path.exists(properties_path):
+            with open(properties_path, "r") as f:
+                for line in f:
+                    if "=" in line:
+                        key, value = line.strip().split("=", 1)
+                        existing[key] = value
+
+        # Actualizar/añadir propiedades
+        existing.update(new_properties)
+
+        # Escribir todas las propiedades
+        with open(properties_path, "w") as f:
+            for key, value in existing.items():
+                f.write(f"{key}={value}\n")
+
+        print("Propiedades guardadas correctamente.")
+        
+        self.loadProperties(self.lastServer)
+        
+    def loadProperties(self,server):
+        self.lastServer = server
+        ruta = os.path.join(base_path, "servers", server)
+        if not os.path.exists(ruta):
+            print(f"El servidor {server} no existe.")
+            return
+        
+        properties_path = os.path.join(ruta, "server.properties")
+        if not os.path.exists(properties_path):
+            print("No se encontró el archivo server.properties.")
+            return
+
+        with open(properties_path, 'r') as f:
+            properties = f.read()
+            print(f"Propiedades del servidor {server}:\n{properties}")
+
+        for line in properties.splitlines():
+            if line.startswith("difficulty="):
+                difficulty = line.split("=")[1].strip()
+                if difficulty == "peaceful":
+                    self.main_window.peacefulBtn.setChecked(True)
+                elif difficulty == "easy":
+                    self.main_window.easyBtn.setChecked(True)
+                elif difficulty == "normal":
+                    self.main_window.normalBtn.setChecked(True)
+                elif difficulty == "hard":
+                    self.main_window.hardBtn.setChecked(True)
+
+            elif line.startswith("gamemode="):
+                gamemode = line.split("=")[1].strip()
+                if gamemode == "survival":
+                    self.main_window.survivalBtn.setChecked(True)
+                elif gamemode == "creative":
+                    self.main_window.creativeBtn.setChecked(True)
+                elif gamemode == "adventure":
+                    self.main_window.adventureBtn.setChecked(True)
+                elif gamemode == "spectator":
+                    self.main_window.spectatorBtn.setChecked(True)
+                    
+            elif line.startswith("max-players="):
+                max_players = line.split("=")[1].strip()
+                self.main_window.players.setValue(int(max_players))
+            elif line.startswith("view-distance="):
+                view_distance = line.split("=")[1].strip()
+                self.main_window.chunks.setValue(int(view_distance))
+            elif line.startswith("server-port="):
+                server_port = line.split("=")[1].strip()
+                self.main_window.portNumber.setValue(int(server_port))
+            elif line.startswith("motd="):
+                motd = line.split("=")[1].strip()
+                self.main_window.serverNameEdit.setText(motd)
+            elif line.startswith("online-mode="):
+                online_mode = line.split("=")[1].strip()
+                self.main_window.onlineMode.setChecked(online_mode.lower() == "true")
+            elif line.startswith("white-list="):
+                white_list = line.split("=")[1].strip()
+                self.main_window.Whitelist.setChecked(white_list.lower() == "true")
+            elif line.startswith("pvp="):
+                pvp = line.split("=")[1].strip()
+                self.main_window.pvp.setChecked(pvp.lower() == "true")
+
+    def insertUserWhiteList(self):
+        user_name = self.main_window.nametagPlayer.text()
+        if user_name:
+            write_list_path = os.path.join(base_path, "servers", self.lastServer, "whitelist.json")
+            if not os.path.exists(write_list_path):
+                with open(write_list_path, 'w') as f:
+                    json.dump([], f)
+            with open(write_list_path, 'r+') as f:
+                white_list = json.load(f)
+                # Verificar si el usuario ya está en la lista
+                if any(user['name'] == user_name for user in white_list):
+                    print(f"El usuario {user_name} ya está en la lista blanca.")
+                    return
+                
+                url = f"https://api.mojang.com/users/profiles/minecraft/{user_name}"
+                
+                response = requests.get(url)
+                if response.status_code != 200:
+                    print(f"❌ Error al obtener el usuario {user_name}: {response.status_code}")
+                    return
+                user_data = response.json()
+                if not user_data:
+                    print(f"❌ Usuario {user_name} no encontrado.")
+                    return
+                user_id = user_data.get("id", user_name)
+
+                user_id = str(uuid.UUID(user_id))  # Convertir a UUID si es necesario
+                user_name = user_data.get("name", user_name)
+                # Añadir el nuevo usuario
+                white_list.append({"uuid": user_id, "name": user_name})
+                f.seek(0)
+                f.write(json.dumps(white_list, indent=4))
+                f.truncate()
+
+            self.reloadWhiteList()
+            self.main_window.nametagPlayer.clear()
+
+    def reloadWhiteList(self):
+        self.main_window.whiteList.clear()
+        ruta = os.path.join(base_path, "servers", self.lastServer)
+        if not os.path.exists(ruta):
+            print(f"El servidor {self.lastServer} no existe.")
+            return
+        
+        white_list_path = os.path.join(ruta, "whitelist.json")
+        if not os.path.exists(white_list_path):
+            print("No se encontró el archivo whitelist.json.")
+            return
+        
+        with open(white_list_path, 'r') as f:
+            white_list = json.load(f)
+        
+        for entry in white_list:
+            nameTag = entry.get('name', 'Unknown')
+            item = QListWidgetItem(nameTag)
+            try:
+                avatar_url = f"https://minotar.net/avatar/{nameTag}/32"
+                avatar_pixmap = QPixmap()
+                avatar_pixmap.loadFromData(requests.get(avatar_url).content)
+                icon = QIcon(avatar_pixmap)
+            except Exception as e:
+                print(f"Error al cargar el avatar de {nameTag}: {e}")
+                icon = QIcon("minecraft/ico/default_avatar.png")
+            item.setIcon(icon)
+            self.main_window.whiteList.addItem(item)
+            
+            
     def reloadServers(self):
+        self.main_window.listServers.clear()
         servidores = os.listdir(base_path + "/servers")
         for server in servidores:
             uriServer = f"{base_path}/servers/{server}"
@@ -36,7 +236,32 @@ class Window(QMainWindow):
             nombre = QLabel(server)
             ico = QIcon(f"{uriServer}/server-icon.png")
             button = QPushButton( "START")
+            folderIcon = QIcon("minecraft/ico/folder.png")
             
+            folderButton = QPushButton(folderIcon, "")
+            folderButton.setStyleSheet("""
+    QPushButton {
+        background-color: #4CAF50;
+        color: white;
+        padding: 10px 20px;
+        border-radius: 10px;
+        font-size: 16px;
+        font-weight: bold;
+        border: 2px solid #388E3C;
+    }
+
+    QPushButton:hover {
+        background-color: #45A049;
+        border: 2px solid #2E7D32;
+    }
+
+    QPushButton:pressed {
+        background-color: #388E3C;
+        border: 2px solid #1B5E20;
+    }
+""")
+
+            version = "N/A"
             
             try:
                 
@@ -44,12 +269,14 @@ class Window(QMainWindow):
                 nombreJar = f"{version}_server_vanilla.jar"
                 rutaJar = os.path.join(base_path, "jars", nombreJar)
             
-
-                button.clicked.connect(lambda _, s=server: self.startServer(s, 1024, 2048, rutaJar))
+                button.clicked.connect(partial(self.startServer, server, 1024, 2048, rutaJar))
+                folderButton.clicked.connect(partial(QDesktopServices.openUrl, QUrl.fromLocalFile(uriServer)))
 
             except :
                 button.setEnabled(False)
                 button.setText("Recargar la APP")
+                # Usar QTimer para recargar la lista después de 100 segundos
+                QTimer.singleShot(100000, self.reloadServers)
                 print(f"No se encontró el JAR para el servidor {server}.")
             img.setPixmap(ico.pixmap(64, 64))
         
@@ -62,16 +289,25 @@ class Window(QMainWindow):
             layout.addLayout(layoutInfo)
             layout.addStretch()
             layout.addWidget(button)
+            layout.addWidget(folderButton)
             widget.setLayout(layout)
         
             item = QListWidgetItem()
+            item.setData(Qt.ItemDataRole.UserRole, server)  # Guardar el nombre del servidor
             item.setSizeHint(widget.sizeHint())
 
             self.main_window.listServers.addItem(item)
             self.main_window.listServers.setItemWidget(item, widget)
-        self.main_window.listServers.setCurrentRow(0)
+        self.main_window.listServers.itemClicked.connect(self.handle_item_click)
+
         
-    def crearBasePath(self):
+    def handle_item_click(self, item):
+        self.main_window.configurePropertiesWidget.setVisible(True)
+        serverName = item.data(Qt.ItemDataRole.UserRole)
+        
+        self.loadProperties(serverName)
+
+    def crearBaseFolders(self):
         if not os.path.exists(base_path):
             os.makedirs(base_path)
             os.makedirs(os.path.join(base_path, "jars"))
@@ -102,16 +338,23 @@ class Window(QMainWindow):
         minRamLabel = QLabel("RAM mínima (MB):")
         minRamSpin = QSpinBox()
         minRamSpin.setRange(512, 32000)
-        minRamSpin.setValue(1024)
+        minRamSpin.setValue(2048)
 
         maxRamLabel = QLabel("RAM máxima (MB):")
         maxRamSpin = QSpinBox()
         maxRamSpin.setRange(512, 64000)
-        maxRamSpin.setValue(2048)
+        maxRamSpin.setValue(4096)
 
+        seedLabel = QLabel("&Seed:")
+        seedEdit = QLineEdit()
+        seedLabel.setBuddy(seedEdit)
         # --- Botones ---
         createButton = QPushButton("Crear")
         cancelButton = QPushButton("Cancelar")
+        
+        # --- Checkbox ---
+        
+        hardcoreCheck = QCheckBox("Hardcore") 
 
         # --- Layouts ---
         formLayout = QFormLayout()
@@ -120,6 +363,8 @@ class Window(QMainWindow):
         formLayout.addRow(tipoLabel, tipoCombo)
         formLayout.addRow(minRamLabel, minRamSpin)
         formLayout.addRow(maxRamLabel, maxRamSpin)
+        formLayout.addRow(seedLabel, seedEdit)
+        formLayout.addRow(hardcoreCheck)
 
         buttonLayout = QHBoxLayout()
         buttonLayout.addStretch()
@@ -138,6 +383,8 @@ class Window(QMainWindow):
             tipoCombo.currentText(),
             minRamSpin.value(),
             maxRamSpin.value(),
+            seedEdit.text(),
+            hardcoreCheck.isChecked(),
             dlg
         ))
         cancelButton.clicked.connect(dlg.reject)
@@ -145,6 +392,18 @@ class Window(QMainWindow):
         
         dlg.exec()
 
+    def writeProperties(self, ruta, text):
+        server_properties_path = os.path.join(ruta, "server.properties")
+        # Si no existe, crea el fichero y escribe la línea
+        if not os.path.exists(server_properties_path):
+            with open(server_properties_path, "w") as f:
+                f.write(text)
+        else:
+            # Si existe, añade la línea al final
+            with open(server_properties_path, "a") as f:
+                f.write(text)
+
+        
     def aceptar_eula(self,base_path, nombre):
 
         server_path = os.path.join(base_path, "servers", nombre)
@@ -180,7 +439,7 @@ class Window(QMainWindow):
         print("EULA añadida y aceptada.")
 
 
-    def crearServidor(self, nombre, version, tipo, ram_min, ram_max, dialog):
+    def crearServidor(self, nombre, version, tipo, ram_min, ram_max,seed, hardcore, dialog):
         if not nombre.strip():
             print("El nombre del servidor no puede estar vacío.")
             return
@@ -196,7 +455,13 @@ class Window(QMainWindow):
 
         # Aquí aceptamos la EULA automáticamente
         self.aceptar_eula(base_path, nombre)
-
+        
+        ruta = os.path.join(base_path, "servers", nombre)
+        if hardcore:
+            self.writeProperties(ruta, "hardcore=true\n")
+        if seed:
+            self.writeProperties(ruta, f"level-seed={seed}\n")
+            
         shutil.copy("minecraft/ico/server-icon.png", f"{base_path}/servers/{nombre}/")
         # Lanzamos el servidor
         self.startServer(nombre, ram_min, ram_max, f"{base_path}/jars/{nombreJar}")
